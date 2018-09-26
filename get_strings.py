9795f69b57b2a4e7e8ef9688dfa95192f834fbe5
Alexander Hart <axr.hart@gmail.com>
2018-09-26 23:50:01 +0200

    added file storage

--- get_strings.py
+++ get_strings.py
@@ -1,5 +1,13 @@
 import urllib2
+import datetime
+import random
+import sys
+import cPickle as pickle
 from bs4 import BeautifulSoup
+
+# Init variables
+size = 10
+append = True
 
 # Useful functions
 
@@ -10,46 +18,80 @@ def rmBrackets(string):
     else:
         return string
 
-# Scrape ML Glossary
-google_page = "https://developers.google.com/machine-learning/glossary/"
-page = urllib2.urlopen(google_page)
-soup = BeautifulSoup(page, 'html.parser')
-entries = soup.find_all('h2', attrs={'class': 'hide-from-toc'})
+# Scrape ML Glossary if not already saved
 
-first_texts = []
+try:
+    with open('ml.pkl', 'rb') as input:
+        first_texts = pickle.load(input)
+except IOError:
+    google_page = "https://developers.google.com/machine-learning/glossary/"
+    page = urllib2.urlopen(google_page)
+    soup = BeautifulSoup(page, 'html.parser')
+    entries = soup.find_all('h2', attrs={'class': 'hide-from-toc'})
 
-for entry in entries:
-    first_texts.append(rmBrackets(entry.text.strip()))
+    first_texts = []
 
-# Scrapy Psych Glossary
+    for entry in entries:
+        first_texts.append(rmBrackets(entry.text.strip()))
 
-spark_page = "https://www.sparknotes.com/psychology/psych101/glossary/terms/"
-page = urllib2.urlopen(spark_page)
-soup = BeautifulSoup(page, 'html.parser')
-lines = soup.find_all('div', attrs={'class': 'content_txt'})
-entries = soup.find_all('b')
+    with open("ml.pkl","wb") as output:
+        pickle.dump(first_texts, output, -1)  
 
-second_texts = []
+# Scrapy Psych Glossary if not already saved
+try:
+    with open('psy.pkl', 'rb') as input:
+        second_texts = pickle.load(input)
+except IOError:        
+    spark_page = "https://www.sparknotes.com/psychology/psych101/glossary/terms/"
+    page = urllib2.urlopen(spark_page)
+    soup = BeautifulSoup(page, 'html.parser')
+    lines = soup.find_all('div', attrs={'class': 'content_txt'})
+    entries = soup.find_all('b')
 
-for entry in entries:
-    second_texts.append(rmBrackets(entry.text.strip().encode('ascii','ignore')))
+    second_texts = []
 
-# Generate output of matches
+    for entry in entries:
+        second_texts.append(rmBrackets(entry.text.strip().encode('ascii','ignore')))
 
-file = open("full_list.md", "w")
-file.write("# List of generated pairs\n\n")
+    with open("psy.pkl","wb") as output:
+        pickle.dump(second_texts, output, -1)  
+
+# Generate output of all matches, if file doesn't exist already
+try:
+    open("full_list.md")
+except IOError:    
+    file = open("full_list.md","w")
+    file.write("# List of generated pairs\n\n")
+    file.close
+
+    file = open("full_list.md", "a")
+    for x in range(0,len(first_texts)-1):
+        file.write("## " + first_texts[x] +"\n")
+        file.write("| One | Two | Three |\n")
+        file.write("| --- | --- | ----- |\n")
+        for y in range(0,len(second_texts)-1):
+            file.write("| " + first_texts[x] + " " + second_texts[y] + " ")
+            if y % 3 == 0:
+                file.write("|\n")
+        
+        file.write("\n")        
+    file.close  
+
+# Generate random output
+if append:
+    file = open("random_list.md","a")
+else:
+    file = open("random_list.md","w")
+
+file.write("# List of generated pairs\n## " + str(datetime.datetime.now()) + "\n\n")   
 file.close
 
-file = open("full_list.md", "a")
-for x in range(0,len(first_texts)-1):
-    file.write("## " + first_texts[x] +"\n")
-    file.write("| One | Two | Three |\n")
-    file.write("| --- | --- | ----- |\n")
-    for y in range(0,len(second_texts)-1):
-        file.write("| " + first_texts[x] + " " + second_texts[y] + " ")
-        if y % 3 == 0:
-            file.write("|\n")
-    
-    file.write("\n")    
-    
-file.close
+file = open("random_list.md","a")
+for x in range(1,11):
+    a = random.randint(0,len(first_texts)-1)
+    b = random.randint(0,len(second_texts)-1)
+
+    file.write(str(x) + ". " + first_texts[a] + " " + second_texts[b] + "\n")
+
+file.write("\n")
+file.close    
---
+++ random_list.md
@@ -0,0 +1,110 @@
+# List of generated pairs
+ ## 2018-09-26 23:18:55.289000
+
+1. TPU worker Fixation
+2. classification model Learning model of addiction
+3. partitioning strategy Temperament
+4. false positive Difference threshold
+5. L2 loss Retina
+6. clustering Mental set
+7. input function Menarche
+8. Estimator Sedatives
+9. least squares regression Timbre
+10. metric Sensory adaptation
+
+# List of generated pairs
+## 2018-09-26 23:19:48.307000
+
+1. sparsity Serotonin
+2. static model Dissociative identity disorder
+3. heuristic Generative
+4. session Subject bias
+5. scoring Optic disk
+6. tf.Example Empty nest
+7. root directory Self-efficacy
+8. hinge loss Content validity
+9. step Experimental group
+10. discriminator Personal unconscious
+
+# List of generated pairs
+## 2018-09-26 23:35:03.514000
+
+1. recommendation system Flashbulb memories
+2. binning Telegraphic speech
+3. sparse feature Catatonic type
+4. false positive Ratio schedule
+5. backpropagation GABA
+6. Rectified Linear Unit Proactive interference
+7. convergence Acrostic
+8. crash blossom Decision-making
+9. confirmation bias Somatization disorder
+10. least squares regression Reticular formation
+
+# List of generated pairs
+## 2018-09-26 23:36:14.972000
+
+1. lambda Reinforcement schedule
+2. static model Dissonance theory
+3. Feature column Monocular cues
+4. sigmoid function Object relations
+5. regularization rate Culture-bound disorders
+6. loss surface Ultradian rhythms
+7. Feature column Avoidant personality disorder
+8. categorical data Bulimia nervosa
+9. Operation Inferiority complex
+10. calibration layer Secondary sex characteristics# List of generated pairs
+## 2018-09-26 23:37:47.586000
+
+1. supervised machine learning Attitudes
+2. normalization Rapid eye movement
+3. weight Problem solving
+4. N-gram Social norms
+5. k-median Client-centered therapy
+6. one-vs.-all Random assignment
+7. training Retrieval cues
+8. recommendation system Implantation
+9. supervised machine learning Gate-control theory
+10. stochastic gradient descent Acetylcholine
+
+# List of generated pairs
+## 2018-09-26 23:38:03.970000
+
+1. partial derivative Flynn effect
+2. bigram Empty nest
+3. reporting bias Nucleotides
+4. upweighting Antagonists
+5. centroid Attitudes
+6. inter-rater agreement Closure
+7. gradient Overconfidence effect
+8. backpropagation Pure light
+9. RNN Retrograde amnesia
+10. dropout regularization Corticosteroids
+
+# List of generated pairs
+## 2018-09-26 23:47:40.776000
+
+1. heuristic Dopamine
+2. learning rate Pinna
+3. softmax Unstable attribution
+4. optimizer Dementia
+5. training Germinal stage
+6. negative class Antisocial personality disorder
+7. neuron Reciprocity norm
+8. offline inference Papillae
+9. prediction Fixed-interval schedule
+10. Momentum Pinna
+
+# List of generated pairs
+## 2018-09-26 23:48:33.481000
+
+1. experimenter's bias Social clocks
+2. synthetic feature Age of viability
+3. boosting Gender stereotypes
+4. TPU node Lateralization
+5. quantization Shaping
+6. implicit bias Hierarchical classification
+7. dense layer Lithium
+8. test set Auditory nerve
+9. inter-rater agreement Pupil
+10. true positive rate Dark adaptation
+
